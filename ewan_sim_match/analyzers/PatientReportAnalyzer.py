90812f61705ae7c2a34b4fb9de593d21cc063d63
moniqueambrose <mambrose@idmod.org>
2022-01-28 10:52:16 -0800

    limit timesteps saved in analyzer

--- ewan_sim_match/analyzers/PatientReportAnalyzer.py
+++ ewan_sim_match/analyzers/PatientReportAnalyzer.py
@@ -1,8 +1,11 @@
 import os
+import sys
 import pandas as pd
 import numpy as np
 from idmtools.entities import IAnalyzer
 from idmtools.entities.simulation import Simulation
+from idmtools.analysis.platform_anaylsis import PlatformAnalysis
+from idmtools.assets import AssetCollection, Asset
 
 from logging import getLogger
 logger = getLogger()
@@ -30,29 +33,30 @@ class PatientAnalyzer(IAnalyzer):
         ntsteps = data[self.filenames[0]]["ntsteps"]
         npatients = len(patients)
         logger.info('Parsing channels %s for %d patients and %d time steps',
-             self.channels, npatients, ntsteps)
+             self.channels, npatients, ntsteps-self.start_report_day)
 
         # fill in long-format dataframe with patient information for each simulation day. columns are:
         #    0) Timestep of simulation
         #    1) Patient ID
         #    2) Patient age
         #    3-X) Channel values for that patient at that timestep
-        patient_info_temp = np.empty((ntsteps, 3+len(self.channels)), dtype='float')
+        patient_info_temp = np.empty((ntsteps, 4+len(self.channels)), dtype='float')
         patient_info_temp[:, 0] = np.arange(start=0, stop=ntsteps, step=1)  # day of simulation
 
-        birthdays = [p['birthday'] for p in patients]
-        initial_ages = [p['initial_age'] for p in patients]
+        birthdays = [np.round(p['birthday'], decimals=0) for p in patients]
+        initial_ages = [np.round(p['initial_age'], decimals=0) for p in patients]
         patient_df_list = []
         for i, p in enumerate(patients):
             patient_info = patient_info_temp.copy()
             patient_info[:, 1] = np.repeat(p['id'], ntsteps)  # patient ID
-            pos_first_data = max(birthdays[i], 0)  # get day of simulation this patient appears in simulation
+            pos_first_data = max(birthdays[i]+1, 0)  # get day of simulation this patient appears in simulation.
             # calculate age of patient on each day of simulation
-            patient_info[pos_first_data:, 2] = np.arange(start=initial_ages[i], stop=(initial_ages[i] + ntsteps - birthdays[i] - 1))
+            patient_info[pos_first_data:, 2] = np.arange(start=initial_ages[i], stop=(initial_ages[i] + ntsteps - pos_first_data))
+            patient_info[:, 3] = np.repeat(birthdays[i], ntsteps)  # patient birthday
             # iterate through channels, adding to data frame
             for c, channel in enumerate(self.channels):
-                patient_info[pos_first_data:, 3+c] = p[channel]
-            patient_df_list.append(pd.DataFrame(data=patient_info, columns=['simday', 'id', 'age'] + self.channels))
+                patient_info[pos_first_data:(pos_first_data + len(p[channel])), 4+c] = p[channel]  # Note: should be able to write "patient_info[pos_first_data:, 4+c]" but there sometimes is an issue with output length being one shorter than the simulation
+            patient_df_list.append(pd.DataFrame(data=patient_info, columns=['simday', 'id', 'age', 'birthday'] + self.channels))
         patient_df = pd.concat(patient_df_list)
 
         # remove any days before the start_report_day
@@ -86,18 +90,44 @@ if __name__ == '__main__':
     from idmtools.core.platform_factory import Platform
     from ewan_sim_match.manifest import analyzed_ouptut_path
 
+    use_ssmt = True
+
     # Set the platform where you want to run your analysis
     # In this case we are running in BELEGOST since the Work Item we are analyzing was run on COMPS
     with Platform('Calculon') as platform:
 
         # Set the experiment you want to analyze
-        experiments = {'test_infectDuration_Navrongo2': 'aaa3fc9f-787a-ec11-a9f3-9440c9be2c51'}
+        experiments = {
+            # 'model_validation_duration_Navrongo_test10y_demog2': '81958f9e-097e-ec11-a9f3-9440c9be2c51',
+            'model_validation_duration_Navrongo_test30y_demog2': '0e54d096-097e-ec11-a9f3-9440c9be2c51'}
+        start_report_day = 365*25
 
-        for dirname, exp_id in experiments.items():
-            # Initialize the analyser class with the path of the output csv file
-            analyzers = [PatientAnalyzer(working_dir=analyzed_ouptut_path, dir_name=dirname, start_report_day=365*85)]
+        if use_ssmt:
+            for dirname, exp_id in experiments.items():
+                # Initialize the SSMT PlatformAnalysis class with all parameters
+                # first add files needed for prevalence_analyzer.py to assetcollection in ssmt
+                ac = AssetCollection()
 
-            # Create AnalyzerManager with required parameters
-            manager = AnalyzeManager(ids=[(exp_id, ItemType.EXPERIMENT)],
-                                     analyzers=analyzers)
-            manager.analyze()
+                analysis = PlatformAnalysis(platform=platform,
+                                            experiment_ids=[exp_id],
+                                            analyzers=[PatientAnalyzer],
+                                            analyzers_args=[{'working_dir': 'outputs',
+                                                             'dir_name': dirname,
+                                                             'start_report_day': start_report_day}],
+                                            analysis_name=os.path.split(sys.argv[0])[1],
+                                            tags={'WorkItem type': 'Docker'},
+                                            asset_files=ac,
+                                            wait_till_done=True)
+                # Run analysis on COMPS
+                analysis.analyze(check_status=True)
+        else:
+            for dirname, exp_id in experiments.items():
+                # Initialize the analyser class with the path of the output csv file
+                analyzers = [PatientAnalyzer(working_dir=analyzed_ouptut_path, dir_name=dirname, start_report_day=start_report_day)]
+
+                # Create AnalyzerManager with required parameters
+                manager = AnalyzeManager(ids=[(exp_id, ItemType.EXPERIMENT)],
+                                         analyzers=analyzers)
+                manager.analyze()
+
+
