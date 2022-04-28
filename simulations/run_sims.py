1b9858b1f7d564e3fe89d639818cd9a937c9ae3f
YeChen-IDM <yechen@idmod.org>
2022-04-28 11:50:58 -0700

    add argparse, update generate_sim()

--- simulations/run_sims.py
+++ simulations/run_sims.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 
 # idmtools ...
 from idmtools.builders import SimulationBuilder
@@ -13,7 +14,7 @@ import simulations.params as params
 from simulations import manifest as manifest
 
 
-def general_sim(sites=None):
+def general_sim(site=None, nSims=2):
     """
     This function is designed to be a parameterized version of the sequence of things we do 
     every time we run an emod experiment. 
@@ -28,10 +29,10 @@ def general_sim(sites=None):
     
     task = EMODTask.from_default2(
             config_path="my_config.json",
-            eradication_path=manifest.eradication_path,
+            eradication_path=str(manifest.eradication_path),
             ep4_custom_cb=None,
             campaign_builder=None,
-            schema_path=manifest.schema_file,
+            schema_path=str(manifest.schema_file),
             param_custom_cb=set_param_fn,
             demog_builder=None,
         )
@@ -40,33 +41,30 @@ def general_sim(sites=None):
     builder = SimulationBuilder()
 
 
-    exp_name = params.exp_name
+    exp_name = "validation_" + site
 
     # Sweep run number
-    builder.add_sweep_definition(update_sim_random_seed, range(params.nSims))
+    builder.add_sweep_definition(update_sim_random_seed, range(nSims))
 
     # Sweep sites and seeds - based on values in simulation_coordinator csv
-    builder.add_sweep_definition(set_simulation_scenario, sites)
+    # builder.add_sweep_definition(set_simulation_scenario, [site])
+    # set_simulation_scenario_for_matched_site = partial(set_simulation_scenario, csv_path=manifest.simulation_coordinator_path)
+    #set_simulation_scenario_for_sweep_site = partial(set_simulation_scenario, csv_path=manifest.simulation_coordinator_path)
+    builder.add_sweep_definition(partial(set_simulation_scenario, csv_path=manifest.simulation_coordinator_path), [site])
 
     # create experiment from builder
     print( f"Prompting for COMPS creds if necessary..." )
     experiment = Experiment.from_builder(builder, task, name=exp_name)
 
     # The last step is to call run() on the ExperimentManager to run the simulations.
-    experiment.run(wait_until_done=True, platform=platform)
-
-    # Check result
-    if not experiment.succeeded:
-        print(f"Experiment {experiment.uid} failed.\n")
-        exit()
-
-    print(f"Experiment {experiment.uid} succeeded.")
+    experiment.run(wait_until_done=False, platform=platform)
 
     # Save experiment id to file
-    with open("COMPS_ID", "w") as fd:
+    with open(f"{site}_COMPS_ID_submit", "w") as fd:
         fd.write(experiment.uid.hex)
     print()
     print(experiment.uid.hex)
+    return experiment.uid.hex
 
 
 if __name__ == "__main__":
@@ -75,6 +73,10 @@ if __name__ == "__main__":
     # print("Retrieving Eradication and schema.json from Bamboo...")
     # get_model_files( plan, manifest )
     # print("...done.")
+    
+    parser = argparse.ArgumentParser(description='Process site name')
+    parser.add_argument('--site', '-s', type=str, help='site name', default='sugungum_1970')#params.sites[0]) # not sure if we want to make this required argument
+    parser.add_argument('--nSims', '-n', type=int, help='number of simulations', default=2)#params.nSims)
+    args = parser.parse_args()
 
-
-    general_sim(sites=params.sites)
+    general_sim(site=args.site, nSims=args.nSims)
