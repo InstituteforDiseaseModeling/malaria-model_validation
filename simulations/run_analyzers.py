5aa237a45503994bded4b4e3828c4a0f715ba6a9
YeChen-IDM <yechen@idmod.org>
2022-05-25 11:35:43 -0700

    run_analyzer script and add analyzer work items with suite id as tag

--- simulations/run_analyzers.py
+++ simulations/run_analyzers.py
@@ -1,6 +1,7 @@
-# use the coordinator csv to determine which analyzers should be run for each site
-import pandas as pd
+import argparse
+import simulations.params as params
 import simulations.manifest as manifest
+from simulations.helpers import get_comps_id_filename, load_coordinator_df, get_suite_id
 
 from idmtools.core.platform_factory import Platform
 from idmtools.analysis.platform_anaylsis import PlatformAnalysis
@@ -9,33 +10,20 @@ from simulations.analyzers.ParDensAgeAnalyzer import ParDensAgeAnalyzer
 from simulations.analyzers.InfectiousnessByParDensAgeAnalyzer import InfectiousnessByParDensAgeAnalyzer
 from simulations.analyzers.PatientReportAnalyzer import PatientAnalyzer
 from simulations.analyzers.MonthlySummaryReportAnalyzer import MonthlySummaryReportAnalyzer
+from simulations.wait_for_experiment import check_experiment
 
 
-# TODO: replace manual specification of exp ids once we are using a single thread to track experiments
-exp_name_id = {
-    # 'validation_navrongo_2000': '71cb33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_sotuba_1999': '0bcc33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_dongubougou_1999': '12cc33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_sugungum_1970': '0ccc33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_ebolakounou_1997': '6ccb33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_dielmo_1990': '6ecb33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_chonyi_1999': '70cb33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_ngerenya_1999': '68cb33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_ndiop_1993': '69cb33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_matsari_1970': '6dcb33c9-e5cc-ec11-a9f8-b88303911bc1',
-    # 'validation_rafin_marke_1970': '6fcb33c9-e5cc-ec11-a9f8-b88303911bc1',
-    'validation_laye_2007': 'ee4df6e2-5ed5-ec11-a9f8-b88303911bc1',
-    'validation_dapelogo_2007': 'f24df6e2-5ed5-ec11-a9f8-b88303911bc1',  # 6bcb33c9-e5cc-ec11-a9f8-b88303911bc1
-    # 'validation_koundou_1997': '67cb33c9-e5cc-ec11-a9f8-b88303911bc1',
-}
+def run_analyzers(site, characteristic=False):
+    platform = Platform(manifest.platform_name)
+    comps_id_file = get_comps_id_filename(site=site)
+    with open(comps_id_file, 'r') as id_file:
+        exp_id = id_file.readline()
+    # Wait for experiment to be done
+    if check_experiment(site, platform):
+        coord_df = load_coordinator_df(characteristic=characteristic, set_index=True)
 
-if __name__ == "__main__":
-    platform = Platform('CALCULON')
-    coord_df = pd.read_csv(manifest.simulation_coordinator_path)
-    coord_df = coord_df.set_index('site')
-
-    for expt_name, id in exp_name_id.items():
-        site = expt_name.replace('validation_', '')
+        # for expt_name, id in exp_name_id.items():
+        # site = expt_name.replace('validation_', '')
         report_start_day = int(coord_df.at[site, 'report_start_day'])
         # determine the analyzers to run for each site
         analyzers = []
@@ -45,20 +33,20 @@ if __name__ == "__main__":
                 analyzers.append(ParDensAgeAnalyzer)
                 analyzer_args.append({'expt_name': site,
                                       'sweep_variables': ['Run_Number', 'Site'],
-                                      'start_year': int(report_start_day/365),
-                                      'end_year': int(coord_df.at[site, 'simulation_duration']/365)})
+                                      'start_year': int(report_start_day / 365),
+                                      'end_year': int(coord_df.at[site, 'simulation_duration'] / 365)})
             if coord_df.at[site, 'infectiousness_to_mosquitos']:
                 analyzers.append(InfectiousnessByParDensAgeAnalyzer)
                 analyzer_args.append({'expt_name': site,
                                       'sweep_variables': ['Run_Number', 'Site'],
-                                      'start_year': int(report_start_day/365),
-                                      'end_year': int(coord_df.at[site, 'simulation_duration']/365)})
+                                      'start_year': int(report_start_day / 365),
+                                      'end_year': int(coord_df.at[site, 'simulation_duration'] / 365)})
             if coord_df.at[site, 'age_prevalence']:
                 analyzers.append(MonthlySummaryReportAnalyzer)
                 analyzer_args.append({'expt_name': site,
                                       'sweep_variables': ['Run_Number', 'Site'],
                                       'start_year': int(report_start_day / 365),
-                                      'end_year': int(coord_df.at[site, 'simulation_duration']/365)})
+                                      'end_year': int(coord_df.at[site, 'simulation_duration'] / 365)})
         if coord_df.at[site, 'include_AnnualMalariaSummaryReport']:
             analyzers.append(AnnualSummaryReportAnalyzer)
             analyzer_args.append({'expt_name': site,
@@ -68,11 +56,33 @@ if __name__ == "__main__":
             analyzer_args.append({'expt_name': site,
                                   'start_report_day': report_start_day})
 
-        analysis = PlatformAnalysis(platform=platform, experiment_ids=[id],
+        analysis = PlatformAnalysis(platform=platform, experiment_ids=[exp_id],
                                     analyzers=analyzers,
                                     analyzers_args=analyzer_args,
                                     analysis_name=site)
 
+        suite_id = get_suite_id()
+        analysis.tags = {'Suite': suite_id}
         analysis.analyze(check_status=True)
+
         wi = analysis.get_work_item()
-        print(wi)
+        analyzers_id_file = get_comps_id_filename(site=site, level=2)
+
+        if wi.succeeded:
+            print(f"Analyzer work item {wi.uid} succeeded.\n")
+            with open(analyzers_id_file, 'w') as id_file:
+                id_file.write(wi.uid.hex)
+        else:
+            print(f"Analyzer work item {wi.uid} failed.")
+
+        return wi.succeeded, wi.uid
+    else:
+        return False, exp_id
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process site name')
+    parser.add_argument('--site', '-s', type=str, help='site name',
+                        default=params.sites[0])  # not sure if we want to make this required argument
+    args = parser.parse_args()
+    run_analyzers(args.site)
