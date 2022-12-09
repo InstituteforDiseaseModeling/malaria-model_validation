85ebaeea8e324f652a251215004b7e70d32a037d
YeChen-IDM <yechen@idmod.org>
2022-12-09 11:35:06 -0800

    update load_input and related code to get subset information from csv file.

--- tests/test_load_inputs.py
+++ tests/test_load_inputs.py
@@ -5,9 +5,10 @@ from simulations.load_inputs import load_sites
 
 class LoadInputsTest(BaseTest):
     def test_load_sites(self):
-        sites, nSims, script_names = load_sites()
+        sites, subsets, nSims, script_names = load_sites()
         self.assertEqual(len(sites), len(nSims))
         self.assertEqual(len(sites), len(script_names))
+        self.assertEqual(len(sites), len(subsets))
 
 
 if __name__ == '__main__':
